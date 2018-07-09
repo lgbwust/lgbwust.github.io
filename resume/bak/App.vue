7bc5072908ff793e3f07a13a900fba4ffc5727ce
lgbwust <1461976922@qq.com>
2018-07-09 21:16:42 +0800

    update resume

---
+++ resume/bak/App.vue
@@ -0,0 +1,372 @@
+<template>
+  <div id="app">
+    <div id="content">
+      <StyleEditor ref="styleEditor" :code="currentStyle"></StyleEditor>
+      <ResumeEditor ref="resumeEditor" :markdown="currentMarkdown" :enableHtml="enableHtml"></ResumeEditor>
+    </div>
+    <div id="foot">
+      <ThankEditor ref="thankEditor" :markdown="currentThankMarkdown" :enableHtml="enableHtml"></ThankEditor>
+    </div>
+  </div>
+</template>
+
+<script>
+  import StyleEditor from './components/StyleEditor'
+  import ResumeEditor from './components/ResumeEditor'
+  import ThankEditor from './components/ThankEditor'
+  import './assets/reset.css'
+
+  export default {
+    name: 'app',
+    components: {
+      StyleEditor,
+      ResumeEditor,
+      ThankEditor
+    },
+    data() {
+      return {
+        interval: 5,
+        currentStyle: '',
+        enableHtml: false,
+        fullStyle: [
+          `/*
+* Inspired by https://github.com/sitexa/anires
+* 大家好，我是刘兵。
+* 我来写一份简历！
+*/
+
+/* 给所有元素加上过渡效果 */
+* {
+  transition: all .1s;
+}
+/* 设置背景颜色 */
+html {
+  color: rgb(222,222,222); background: rgb(0,64,64);
+}
+#content{
+  height:70vh;
+  margin:0;
+}
+#foot{
+  height:29vh;
+  margin:0;
+}
+
+/* 设置边框 */
+.styleEditor {
+  padding: .5em;
+  border: 1px solid;
+  margin: .5em;
+  overflow: auto;
+  width: 50vw; height: 70vh;
+  background: rgb(20,20,20);
+}
+/* 代码高亮 */
+.token.selector{ color: rgb(130,150,0); }
+.token.property{ color: rgb(190,140,0); }
+.token.punctuation{ color: yellow; }
+.token.function{ color: rgb(40,160,150); }
+
+/* 加3D效果 */
+html{
+  perspective: 1000px;
+}
+.styleEditor {
+  position: fixed; left: 0; top: 0;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(10deg) translateZ(-100px) ;
+          transform: rotateY(10deg) translateZ(-100px) ;
+}
+
+/* 准备一个编辑器 */
+.resumeEditor{
+  position: fixed; right: 0; top: 0;
+  padding: .5em;  margin: .5em;
+  width: 50vw; height: 70vh;
+  border: 1px solid;
+  background: rgb(200,200,200); color: #222;
+  overflow: auto;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateY(-10deg) translateZ(-100px) ;
+          transform: rotateY(-10deg) translateZ(-100px) ;
+}
+/* 开始写简历 */
+`, `
+/*将Markdown格式翻译成HTML
+ *再对HTML加点样式
+*/
+.resumeEditor{
+  padding: 2em;
+}
+.resumeEditor h2{
+  display: inline-block;
+  border-bottom: 1px solid;
+  margin: 1em 0 .5em;
+}
+.resumeEditor ul,.resumeEditor ol{
+  list-style: none;
+}
+.resumeEditor ul> li::before{
+  content: '•';
+  margin-right: .5em;
+}
+.resumeEditor ol {
+  counter-reset: section;
+}
+.resumeEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, ".") " ";
+  margin-right: .5em;
+}
+.resumeEditor blockquote {
+  margin: 1em;
+  padding: .5em;
+  background: #ddd;
+}
+`, `/* 写封感谢信。
+ * 感谢大家对我的关注。
+ */
+.styleEditor{
+    width:50vw;height:70vh;
+}
+
+.resumeEditor{
+   width:50vw;height:70vh;
+}
+
+.thankEditor{
+  position: relative; left: 0; top: 0;
+  background: #E9D9BB;
+  color: #001C42;
+  overflow: auto;
+}
+
+.thankEditor {
+  width: 99vw; height: 45vh;
+  border: 1px solid #ccc;
+  font-size: .9em;
+}
+`,`
+.thankEditor{
+  padding: .5em;  margin: .5em; margin-top:1em;
+}
+
+.thankEditor ul,.thankEditor ol{
+  list-style: none;
+}
+.thankEditor ul> li::before{
+  content: '☞'; color: red;
+  margin-right: .5em;
+}
+.thankEditor ol {
+  counter-reset: section;
+}
+.thankEditor ol li::before {
+  counter-increment: section;
+  content: counters(section, "☞") " ";
+  margin-right: .5em;
+}
+
+.thankEditor{
+  width: 99vw; height: 45vh;
+  -webkit-transition: none;
+  transition: none;
+  -webkit-transform: rotateX(-10deg) translateZ(-200px);
+          transform: rotateX(-10deg) translateZ(-200px);
+}
+
+`],
+        currentMarkdown: '',
+        currentThankMarkdown: '',
+        fullMarkdown: `刘兵
+====
+坐标：湖北武汉。
+
+职场小白，现在为一名攻城狮。
+
+技能
+====
+
+数据库设计
+----
+  - MySQL、SQL server、Oracle
+  - Redis
+
+后端开发
+----
+  - Java
+  - SpringMVC、 Spring Cloud、 Spring Boot
+  - React、 Ant Design Pro
+  - Jquery
+  - CSS、HTML
+
+前端开发
+----
+  - Web前端开发
+  - JQuery、React
+
+项目经历
+----
+  - 厦门马拉松-指挥调度
+  - [联投商业微信商城](http://www.uigbusiness.com/uims)
+  - [珠海民生数据开放平台](http://data.zhuhai.gov.cn)
+  - 光谷交通
+  - 武汉综合保税区智慧园区项目
+
+技术及语言
+----
+  - Java: SpringMVC, SpringCloud, Hibernate, iBatis, freemarker
+  - Node.js: react
+  - Golang: 小白
+  - Python: 小白
+  - DotNet: 小白
+  - DB: SQLServer, Oracle, MySQL/MariaDB, redis
+  - WebServer: apache, nginx, tomcat
+  - OS: Ubuntu, CentOS, Windows
+  - Others: Docker, git, CSS, HTML, JQuery
+
+工作经历
+====
+
+1. 武汉烽火信息集成技术有限公司
+
+教育经历
+====
+
+1. 武汉科技大学 计算机科学与技术 工学学士
+2. 武汉科技大学 计算机科学与技术 工学硕士
+
+文章
+====
+
+* 暂无
+
+链接
+====
+
+* [GitHub](https://github.com/lgbwust)
+* [技术博客](https://lgbwust.github.io)
+
+联系方式
+====
+
+* E-mail：iceliume@gmail.com
+
+鸣谢
+----
+
+* 暂无
+
+  `
+      }
+    },
+    created() {
+      this.makeResume()
+    },
+
+    methods: {
+      makeResume: async function () {
+        await this.progressivelyShowStyle(0);
+        await this.progressivelyShowResume();
+        await this.progressivelyShowStyle(1);
+        await this.showHtml();
+        await this.progressivelyShowStyle(2);
+        await this.progressivelyShowThanks();
+        await this.progressivelyShowStyle(3)
+      },
+      showHtml() {
+        return new Promise((resolve, reject) => {
+          this.enableHtml = true
+          resolve()
+        })
+      },
+      progressivelyShowStyle(n) {
+        return new Promise((resolve, reject) => {
+          let interval = this.interval
+          let showStyle = (async function () {
+            let style = this.fullStyle[n]
+            if (!style) {
+              return
+            }
+            // 计算前 n 个 style 的字符总数
+            let length = this.fullStyle.filter((_, index) => index <= n).map((item) => item.length).reduce((p, c) => p + c, 0)
+            let prefixLength = length - style.length
+            if (this.currentStyle.length < length) {
+              let l = this.currentStyle.length - prefixLength
+              let char = style.substring(l, l + 1) || ' '
+              this.currentStyle += char
+              if (style.substring(l - 1, l) === '\n' && this.$refs.styleEditor) {
+                this.$nextTick(() => {
+                  this.$refs.styleEditor.goBottom()
+                })
+              }
+              setTimeout(showStyle, interval)
+            } else {
+              resolve()
+            }
+          }).bind(this)
+          showStyle()
+        })
+      },
+      progressivelyShowResume() {
+        return new Promise((resolve, reject) => {
+          let length = this.fullMarkdown.length
+          let interval = this.interval
+          let showResume = () => {
+            if (this.currentMarkdown.length < length) {
+              this.currentMarkdown = this.fullMarkdown.substring(0, this.currentMarkdown.length + 1)
+              let lastChar = this.currentMarkdown[this.currentMarkdown.length - 1]
+              let prevChar = this.currentMarkdown[this.currentMarkdown.length - 2]
+              if (prevChar === '\n' && this.$refs.resumeEditor) {
+                this.$nextTick(() => this.$refs.resumeEditor.goBottom())
+              }
+              setTimeout(showResume, interval)
+            } else {
+              resolve()
+            }
+          }
+          showResume()
+        })
+      },
+      progressivelyShowThanks() {
+        return new Promise((resolve, reject) => {
+          let length = this.thanksMarkdown.length
+          let interval = this.interval
+          let showThanks = () => {
+            if (this.currentThankMarkdown.length < length) {
+              this.currentThankMarkdown = this.thanksMarkdown.substring(0, this.currentThankMarkdown.length + 1)
+              let lastChar = this.currentThankMarkdown[this.currentThankMarkdown.length - 1]
+              let prevChar = this.currentThankMarkdown[this.currentThankMarkdown.length - 2]
+              if (prevChar === '\n' && this.$refs.thankEditor) {
+                this.$nextTick(() => this.$refs.thankEditor.goBottom())
+              }
+              setTimeout(showThanks, interval)
+            } else {
+              resolve()
+            }
+          }
+          showThanks()
+        })
+      }
+    }
+  }
+
+</script>
+
+<style scoped>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  html {
+    min-height: 100vh;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+</style>
